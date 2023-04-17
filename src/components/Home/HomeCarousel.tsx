87cbbb42134edebc98e4c18c4abdb3c566330821
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-04-18 00:18:58 +0900

    correct image link

--- src/components/Home/HomeCarousel.tsx
+++ src/components/Home/HomeCarousel.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const itemList = [
   {
     id: 111,
-    src: '/public/img_shop_fashion.jpeg',
+    src: '/img_shop_fashion.jpeg',
     alt: 'fashion',
     title: '물빠진 청바지!',
     desc: '이제 막 도착한 패션 청바지를 구경해 보세요.',
@@ -14,7 +14,7 @@ const itemList = [
   },
   {
     id: 222,
-    src: '/public/img_shop_digital.jpeg',
+    src: '/img_shop_digital.jpeg',
     alt: 'digital',
     title: '신속한 업무처리!',
     desc: '다양한 디지털 상품을 둘러보세요.',
@@ -22,7 +22,7 @@ const itemList = [
   },
   {
     id: 333,
-    src: '/public/img_shop_grocery.jpeg',
+    src: '/img_shop_grocery.jpeg',
     alt: 'grocery',
     title: '신선한 식품!',
     desc: '농장 직배송으로 더욱 신선한 식료품을 만나보세요.',
--- src/icons/BlogIcon.tsx
+++ src/icons/BlogIcon.tsx
@@ -1,10 +1,5 @@
 export default function BlogIcon() {
   return (
-    <img
-      src="/public/blog_icon.png"
-      alt="naver blog"
-      width="24px"
-      height="24px"
-    />
+    <img src="/blog_icon.png" alt="naver blog" width="24px" height="24px" />
   );
 }
